c03772792d12fde686a0fb154ecbebf03e2f4cde
PanikRacker <PanikRacker@gmail.com>
2023-10-14 21:06:27 +0200

    backup spoolman db after print end

---
+++ spoolman.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+spoolman_folder=~/printer_data/config/Spoolman/data
+branch=master
+
+#####################################################################
+#####################################################################
+
+
+#####################################################################
+################ !!! DO NOT EDIT BELOW THIS LINE !!! ################
+#####################################################################
+grab_version(){
+  if [ ! -z "$spoolman_folder" ]; then
+    cd "$spoolman_folder"
+    spoolman_commit=$(git rev-parse --short=7 HEAD)
+    m1="Spoolman on commit: $spoolman_commit"
+    cd ..
+  fi
+}
+
+push_config(){
+  cd $spoolman_folder
+  git pull origin $branch --no-rebase
+  git add .
+  current_date=$(date +"%Y-%m-%d %T")
+  git commit -m "Autocommit from $current_date" -m "$m1"
+  git push origin $branch
+}
+
+grab_version
+push_config
