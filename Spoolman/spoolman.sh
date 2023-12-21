8a2cadac3dec65f5767481c078a776a6ea388738
PanikRacker <PanikRacker@gmail.com>
2023-12-21 20:33:34 +0100

    Revert "Autocommit from 2023-12-21 20:28:14"
    This reverts commit 78d00e64d60321750fb3f4f968e41c7f9751b75e.

---
+++ Spoolman/spoolman.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+spoolman_folder=~/printer_data/config/Spoolman/data
+branch=master
+
+grab_version(){
+  if [ ! -z "$spoolman_folder" ]; then
+    cd "$spoolman_folder"
+    spoolman_commit=$(git rev-parse --short=7 HEAD)
+    m1="Spoolman on commit: $spoolman_commit"
+    cd ..
+  fi
+}
+
+push_config(){
+  cd $spoolman_folder
+  git pull origin $branch --no-rebase
+  git add .
+  current_date=$(date +"%Y-%m-%d %T")
+  git commit -m "Backup Spoolman $current_date" -m "$m1"
+  git push origin $branch
+}
+
+grab_version
+push_config
